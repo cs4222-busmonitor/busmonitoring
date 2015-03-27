a3e53c613af145aaf02b65d623cca3fd0d7accf3
tanwlj1 <tanwlj1@hotmail.com>
2015-03-28 02:48:14 +0800

    uploaded sensorapp

--- sensorapp/src/androidTest/java/com/example/jeremy/sensorapp/ApplicationTest.java
+++ sensorapp/src/androidTest/java/com/example/jeremy/sensorapp/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.sensorapp;
+package com.example.jeremy.sensorapp;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- sensorapp/src/main/java/com/example/jeremy/sensorapp/MainActivity.java
+++ sensorapp/src/main/java/com/example/jeremy/sensorapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.sensorapp;
+package com.example.jeremy.sensorapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
